63e2af667efa8ae40c179bdee3644217c7d0da31
Naseong Boo <nsboo@autocrypt.io>
2025-04-25 12:38:35 +0900

    change event listener async

--- src/main/java/io/reflectoring/buckpal/adapter/in/internal/AuthCertRequestEventHandler.java
+++ src/main/java/io/reflectoring/buckpal/adapter/in/internal/AuthCertRequestEventHandler.java
@@ -4,6 +4,7 @@ import io.reflectoring.buckpal.application.domain.model.AuthCertRequestEvent;
 import io.reflectoring.buckpal.application.domain.service.AuthCertGenerationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 public class AuthCertRequestEventHandler {
     private final AuthCertGenerationService authCertGenerationService;
 
+    @Async // @Configuration에서 @EnableAsync 필요
     @EventListener
     public void generateAuthCert(AuthCertRequestEvent event) {
         authCertGenerationService.generate(event.getAuthCertRequest());
