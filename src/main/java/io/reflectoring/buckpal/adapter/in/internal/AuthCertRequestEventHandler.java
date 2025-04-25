7f5609572a42daaf2f36c6350b2257e9ee3a891c
Naseong Boo <nsboo@autocrypt.io>
2025-04-25 12:41:27 +0900

    change event listener to transactional

--- src/main/java/io/reflectoring/buckpal/adapter/in/internal/AuthCertRequestEventHandler.java
+++ src/main/java/io/reflectoring/buckpal/adapter/in/internal/AuthCertRequestEventHandler.java
@@ -3,17 +3,15 @@ package io.reflectoring.buckpal.adapter.in.internal;
 import io.reflectoring.buckpal.application.domain.model.AuthCertRequestEvent;
 import io.reflectoring.buckpal.application.domain.service.AuthCertGenerationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
 public class AuthCertRequestEventHandler {
     private final AuthCertGenerationService authCertGenerationService;
 
-    @Async // @Configuration에서 @EnableAsync 필요
-    @EventListener
+    @TransactionalEventListener
     public void generateAuthCert(AuthCertRequestEvent event) {
         authCertGenerationService.generate(event.getAuthCertRequest());
     }
